e89423bf878b8fc0a4fa9897358596e70e4898cb
Jared Lowery <jadersleet@gmail.com>
2017-03-03 17:52:56 -0800

    Updated the model to work with his new user service.

--- src/main/java/data/Model.java
+++ src/main/java/data/Model.java
@@ -5,16 +5,12 @@
  */
 package data;
 
-import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,7 +24,9 @@ import objects.User;
 public class Model {
     static final Logger logger = Logger.getLogger(Model.class.getName());
     private static Model instance;
+    private static Model instancemock;
     private Connection conn;
+    private static String dbConnUrl = System.getenv("JDBC_DATABASE_URL");
     
     public static Model singleton() throws Exception {
         if (instance == null) {
@@ -37,16 +35,37 @@ public class Model {
         return instance;
     }
     
+    public static Model mockSingleton(Connection conn) throws Exception {
+        if (instancemock == null) {
+            instancemock = new Model(conn);
+        }
+        return instancemock;
+    }
+    
+    
     Model() throws Exception
-    {
+    {  
         Class.forName("org.postgresql.Driver");
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        if ((dbUrl == null) || (dbUrl.length() < 1))
-            dbUrl = System.getProperties().getProperty("DBCONN");
-        logger.log(Level.INFO, "dbUrl=" + dbUrl);  
+        logger.log(Level.INFO, "dbUrl=" + getDBConnURL());  
         logger.log(Level.INFO, "attempting db connection");
-        conn = DriverManager.getConnection(dbUrl);
-        logger.log(Level.INFO, "db connection ok.");
+        try
+        {
+            conn = DriverManager.getConnection(getDBConnURL());
+            logger.log(Level.INFO, "db connection ok.");
+        }
+        catch (Exception e)
+        {
+            logger.log(Level.SEVERE, "Unable to open db connection:" + e.toString());
+        }
+    }
+    
+    Model(Connection mockconn) throws Exception
+    {  
+        Class.forName("org.postgresql.Driver");
+        logger.log(Level.INFO, "dbUrl=" + getDBConnURL());  
+        logger.log(Level.INFO, "attempting mock db connection");
+        conn = mockconn;
+        logger.log(Level.INFO, "mock db connection ok.");
     }
     
     private Connection getConnection()
@@ -88,7 +107,19 @@ public class Model {
         return null;
     }
     
-    public int newUser(User usr) throws SQLException
+    public static String getDBConnURL() {
+        if ((dbConnUrl == null) || (dbConnUrl.length() < 1))
+            dbConnUrl = System.getProperties().getProperty("DBCONN");        
+        logger.log(Level.INFO, "dbConnUrl VALUE=" + dbConnUrl);
+        logger.log(Level.INFO, "sys-prop-getprop DBCONN VALUE=" + System.getProperties().getProperty("DBCONN"));
+        return dbConnUrl;
+    }
+    
+    public static void setDBConnURL(String connUrl) {
+        dbConnUrl = connUrl;
+    }
+            
+    public User newUser(User usr) throws SQLException
     {
         String sqlInsert="insert into users (name, age) values ('" + usr.getName() + "'" + "," + usr.getAge() + ");";
         Statement s = createStatement();
@@ -97,11 +128,12 @@ public class Model {
         logger.log(Level.INFO, "statement executed.  atempting get generated keys");
         ResultSet rs = s.getGeneratedKeys();
         logger.log(Level.INFO, "retrieved keys from statement");
-        int userid = -1;
+        int userid = -1;  
         while (rs.next())
             userid = rs.getInt(3);   // assuming 3rd column is userid
         logger.log(Level.INFO, "The new user id=" + userid);
-        return userid;
+        usr.setUserId(userid);
+        return usr;
     }
     
     public void deleteUser(int userid) throws SQLException
@@ -112,10 +144,11 @@ public class Model {
         pst.execute();
     }
     
-    public User[] getUsers() throws SQLException
+    public User[] getUsers(int userId) throws SQLException
     {
         LinkedList<User> ll = new LinkedList<User>();
-        String sqlQuery ="select * from users;";
+        String sqlQuery ="select * from users";
+        sqlQuery += (userId > 0) ? " where userid=" + userId + " order by userid;" : " order by userid;";
         Statement st = createStatement();
         ResultSet rows = st.executeQuery(sqlQuery);
         while (rows.next())
